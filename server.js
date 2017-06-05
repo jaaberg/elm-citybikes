bf33fbf4951c38c623246d7b5445e557e674016f
Jørgen Aaberg <aaberg89@gmail.com>
2017-06-05 14:03:16 +0200

    Add multiple city bike stations and bus stations

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const axios = require('axios');
 
 let app = express();
 
+app.use(express.static('./dist'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './dist', 'index.html'));
+});
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', process.env.SERVER || 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -16,18 +22,62 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/available', function(req, res, next) {
-  axios.get("https://oslobysykkel.no/api/v1/stations/availability", {
+function filterCycleStations(station) {
+  return station.id == "257" || station.id == "262";
+}
+
+function mapCycleStations(station) {
+  return {
+    name: station.id == '257' ? "Trondheimsveien" : "Sofienberg",
+    bikes: station.availability.bikes,
+    locks: station.availability.locks
+  }
+}
+
+function filterDepartures(departure) {
+  return (departure.MonitoredVehicleJourney.LineRef == "31" || departure.MonitoredVehicleJourney.LineRef == "17")
+      && departure.MonitoredVehicleJourney.DirectionRef == "2";
+}
+
+function mapDepartures(departure) {
+  return {
+    lineNumber : departure.MonitoredVehicleJourney.LineRef,
+    destinationName : departure.MonitoredVehicleJourney.DestinationName,
+    expectedDepartureTime : new Date(departure.MonitoredVehicleJourney.MonitoredCall.ExpectedDepartureTime).getTime()
+  }
+}
+
+function fetchCityBikeCycles() {
+  return axios.get("https://oslobysykkel.no/api/v1/stations/availability", {
     headers: {'Client-Identifier': process.env.OSLO_BYSYKKEL_CLIENT_IDENTIFIER}
-  })
-  .then(function (response) {
-    const trondheimsVeien = response.data.stations
-      .filter(station => station.id == "257");
-    res.status(200).send(trondheimsVeien[0]);
-  })
-  .catch(function (error) {
-    console.log(error);
   });
+}
+
+function fetchBuses() {
+  return   axios.get("http://reisapi.ruter.no/StopVisit/GetDepartures/3010533");
+}
+
+app.get('/publictransportation', function(req, res, next) {
+  axios.all([fetchCityBikeCycles(), fetchBuses()])
+  .then(axios.spread(function (cycleResponse, departuresSofienbergResponse) {
+    const bikes = cycleResponse.data.stations
+      .filter(filterCycleStations)
+      .map(mapCycleStations);
+    const departuresSofienberg = departuresSofienbergResponse.data
+      .filter(filterDepartures)
+      .map(mapDepartures)
+      .slice(0, 5);
+    const busStations = [{
+      name: "Sofienberg",
+      departures: departuresSofienberg
+    }];
+
+    res.status(200).send({
+      cityBikeStations: bikes,
+      busStations: busStations
+    })
+  }));
+
 });
 
 http.createServer(app).listen(process.env.PORT || 3001, () => {
--- src/App.elm
+++ src/App.elm
@@ -1,48 +1,76 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
+import Html exposing (Html, text, div, img, span)
+import Html.Attributes exposing (class)
 import Http
 import Json.Decode as Decode
+import Time exposing (every, Time)
+import Date.Extra exposing (toFormattedString)
+import Date exposing (..)
 
 
 type alias Model =
-    { available : Int
+    { cityBikeStations : List CityBikeStation
+    , busStations : List BusStation
+    }
+
+
+type alias CityBikeStation =
+    { name : String
+    , bikes : Int
     , locks : Int
+    }
+
+
+type alias BusStation =
+    { name : String
+    , departures : List BusDeparture
+    }
+
+
+type alias BusDeparture =
+    { lineNumber : String
+    , destinationName : String
+    , expectedDepartureTime : Int
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init flags =
-    ( { available = 0, locks = 0 }
+    ( Model [] []
     , getStatus
     )
 
 
 type Msg
     = NoOp
-    | Alive (Result Http.Error Model)
+    | TrasportationResult (Result Http.Error Model)
+    | GetStatus Time.Time
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Alive (Ok res) ->
-            ( { model | available = res.available, locks = res.locks }, Cmd.none )
+        TrasportationResult (Ok res) ->
+            ( { model | cityBikeStations = res.cityBikeStations, busStations = res.busStations }, Cmd.none )
 
         NoOp ->
             ( model, Cmd.none )
 
-        Alive (Err _) ->
+        TrasportationResult (Err _) ->
             ( model, Cmd.none )
+
+        GetStatus time ->
+            ( model, getStatus )
 
 
 getStatus : Cmd Msg
 getStatus =
     let
         url =
-            "http://localhost:3001/available"
+            "http://localhost:3001/publictransportation"
     in
-        Http.send Alive
+        Http.send TrasportationResult
             (Http.get url decodeResponse)
 
 
@@ -50,18 +78,89 @@ decodeResponse : Decode.Decoder Model
 decodeResponse =
     Decode.map2
         Model
-        (Decode.at [ "availability", "bikes" ] Decode.int)
-        (Decode.at [ "availability", "locks" ] Decode.int)
+        (Decode.at [ "cityBikeStations" ] decodeCityBikeStations)
+        (Decode.at [ "busStations" ] decodeBusStations)
+
+
+decodeBusStations : Decode.Decoder (List BusStation)
+decodeBusStations =
+    Decode.list decodeBusStation
+
+
+decodeBusStation : Decode.Decoder BusStation
+decodeBusStation =
+    Decode.map2
+        BusStation
+        (Decode.at [ "name" ] Decode.string)
+        (Decode.at [ "departures" ] (Decode.list decodeBusDepatures))
+
+
+decodeBusDepatures : Decode.Decoder BusDeparture
+decodeBusDepatures =
+    Decode.map3
+        BusDeparture
+        (Decode.at [ "lineNumber" ] Decode.string)
+        (Decode.at [ "destinationName" ] Decode.string)
+        (Decode.at [ "expectedDepartureTime" ] Decode.int)
+
+
+decodeCityBikeStations : Decode.Decoder (List CityBikeStation)
+decodeCityBikeStations =
+    Decode.list decodeCityBikeStation
+
+
+decodeCityBikeStation : Decode.Decoder CityBikeStation
+decodeCityBikeStation =
+    Decode.map3
+        CityBikeStation
+        (Decode.at [ "name" ] Decode.string)
+        (Decode.at [ "bikes" ] Decode.int)
+        (Decode.at [ "locks" ] Decode.int)
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text ("Available: " ++ (toString model.available)) ]
-        , div [] [ text ("Locks: " ++ (toString model.locks)) ]
+    div [ class "main" ]
+        [ div [] (List.map renderCityBikeStation model.cityBikeStations)
+        , div [ class "bus-departures" ]
+            [ div [ class "content" ] (List.map renderBusStation model.busStations)
+            ]
         ]
+
+
+renderCityBikeStation : CityBikeStation -> Html Msg
+renderCityBikeStation cbs =
+    div [ class "city-bike-station" ]
+        [ div [ class "header" ] [ span [ class "header__border" ] [ text cbs.name ] ]
+        , div [ class "content" ]
+            [ div [] [ text ("Ledige sykler: " ++ (toString cbs.bikes)) ]
+            , div [] [ text ("Ledige låser: " ++ (toString cbs.locks)) ]
+            ]
+        ]
+
+
+renderBusStation : BusStation -> Html Msg
+renderBusStation bs =
+    div [ class "bus-departures" ]
+        [ div [ class "header" ] [ span [ class "header__border" ] [ text bs.name ] ]
+        , div [ class "content" ] (List.map renderBusDeparture bs.departures)
+        ]
+
+
+renderBusDeparture : BusDeparture -> Html Msg
+renderBusDeparture bd =
+    let
+        departureTime =
+            (toFormattedString "HH:mm" (Date.fromTime (toFloat bd.expectedDepartureTime)))
+    in
+        div []
+            [ div []
+                [ text (bd.lineNumber ++ " " ++ bd.destinationName ++ " ")
+                , span [ class "city-bike-station--departure-time" ] [ text departureTime ]
+                ]
+            ]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    every 30000 GetStatus
